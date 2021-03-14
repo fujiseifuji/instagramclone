dd27aefd2dbfb0de816bebcd997e97c71251c2a9
fuji <fujifujiseisei@gmail.com>
2021-03-14 17:18:59 +0900

    Additional: favorite and comment function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
   resources :posts, only: %i(new create index show destroy) do
-    resources :photos, only: %i(create)
+    resources :photos,   only: %i(create)
+    resources :likes,    only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
   end
 end
